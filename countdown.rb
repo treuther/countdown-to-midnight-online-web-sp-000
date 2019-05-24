af241ffb103c33fc74da0bfbca75e2356d69f333
Tracy Reuther <tracyreuther@gmail.com>
2019-05-24 00:33:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,8 +12,8 @@ def countdown_with_sleep(number)
   number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
-    sleep(2)
     number -= 1
+      sleep(2)
   end
   return "HAPPY NEW YEAR!"
 end
